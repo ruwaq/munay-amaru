c92a4229c0bd295e735326008c67fd2138b7101b
Ruwaq <pepepop2000@gmail.com>
2026-08-17 00:09:01 +0300

    feat: vincular dominio personalizado Cloudflare munay.flowclicker.cc

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://munay-amaru.pages.dev',
+  site: 'https://munay.flowclicker.cc',
   output: 'static',
   build: {
     format: 'directory'
