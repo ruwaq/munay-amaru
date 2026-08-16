7f71376fa01133629ed81fd9197e2a1f6ace0ec6
Ruwaq <pepepop2000@gmail.com>
2026-08-17 00:11:06 +0300

    chore: restaurar dominio canonico a https://munay-amaru.pages.dev

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://munay.flowclicker.cc',
+  site: 'https://munay-amaru.pages.dev',
   output: 'static',
   build: {
     format: 'directory'
